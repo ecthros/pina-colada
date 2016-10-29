83a85e1fae03d099d77d1952ce5dd1f04520900e
George Hughey <ghughey@umd.edu>
2016-10-29 18:41:41 +0000

    Scanning now updates database

--- capabilities/scan/syn.py
+++ capabilities/scan/syn.py
@@ -1,5 +1,7 @@
 from capability import *
 from scapy.all import *
+import time
+import datetime
 
 class syn(Capability):
 
@@ -9,9 +11,9 @@ class syn(Capability):
         self.intro = GOOD + "Using Syn Scan module..."
         self.core = core
         self.options = { 
-                "target":       Option("target", "", "target device to scan", True),
-                "start":    Option("start", "", "beginning port of scan", True),
-                "end":      Option("end", "", "ending port of range", True),
+                "target":   Option("target", "10.0.0.57", "target device to scan", True),
+                "start":    Option("start", "0", "beginning port of scan", True),
+                "end":      Option("end", "100", "ending port of range", True),
                 }
         self.help_text = "Scans a computer on a specific port."
 
@@ -24,6 +26,17 @@ class syn(Capability):
                     rep.append(r.sprintf("%sport%"))
         for response in rep:
             print str(response) + " is open"
+
+        print "Updating database..."
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        ports = ','.join(rep)
+        self.core.cur.execute(" \
+            UPDATE computers SET ip='{0}', ports='{1}', last_online='{2}' WHERE ip='{3}'; \
+            INSERT INTO computers(ip,ports,last_online) SELECT '{4}', '{5}', '{6}' \
+            WHERE NOT EXISTS (SELECT 1 FROM computers WHERE ip='{7}')" \
+            .format(target, ports, st, target, target, ports, st, target))
+
         return rep
 
 
