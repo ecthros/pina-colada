6b3a2737f5d93bb32850dfbd202dafdc196ecd67
Srujan Thotapally <srujant@gmail.com>
2016-11-13 19:21:37 -0500

    Started flask front end. CD into pinacolada-website and run the run.py script.

--- frontend/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http')
-var parseUrl = require('parseurl')
-var send = require('send')
- 
-var app = http.createServer(function onRequest (req, res) {
-  send(req, parseUrl(req).pathname).pipe(res)
-}).listen(3000)
