51903e5295d372ce59065eb51dbbc7f7a8a690a3
George Hughey <ghughey@umd.edu>
2016-11-22 02:38:44 +0000

    Fixed tests

--- tests.py
+++ tests.py
@@ -1,4 +1,11 @@
 import os
+import pexpect
+from colorama import *
+import re
+
+prompt = Fore.BLUE + ">> " + Fore.RESET
 
 def base_test():
-    os.system("python cli.py")
+    cli = pexpect.spawn("sudo python cli.py")
+    cli.expect(re.escape(prompt))
+
