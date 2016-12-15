672ca4e3c01644dd611664ddf7474efa9dc2bc57
Kkevsterrr <rstudio@example.com>
2016-12-14 21:43:06 -0500

    cleanup old

--- old_pinacolada-website/run.py
+++
@@ -1,2 +0,0 @@
-from web import app
-app.run(debug=True)
--- old_pinacolada-website/web/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-from pinacolada_website.views import index
-app.register_blueprint(index.mod)
